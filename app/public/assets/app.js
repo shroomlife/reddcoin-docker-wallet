7f00c2e8d57d3564af32f656681f3e0abf27d6c2
shroomlife <robin@shroomlife.de>
2019-01-22 17:06:11 +0100

    removed loader image

--- app/public/assets/app.js
+++ app/public/assets/app.js
@@ -2,36 +2,32 @@ var app = angular.module('WalletApp', []);
 
 app.controller('WalletController', function($scope, $http) {
 
-	let loader = [];
 	let balance = 0;
 
-	let pingLoad = $http.get("/api/ping").then(function() {
+	$http.get("/api/ping").then(function() {
 		$scope.color = "green";
 		$scope.state = 'online';
-
-
-
 	}).catch(function() {
 		$scope.color = "red";
 		$scope.state = 'offline';
 	});
 
-	let balanceLoad = $http.get("/api/getbalance").then(function(response) {
+	$http.get("/api/getbalance").then(function(response) {
 		console.log(response.data);
 		$scope.balance = response.data.balance;
 	});
 
-	let accountsLoad = $http.get("/api/listaccounts").then(function(response) {
+	$http.get("/api/listaccounts").then(function(response) {
 		console.log(response.data);
 		$scope.accounts = response.data.accounts;
 	});
 
-	let transactionsLoad = $http.get("/api/listtransactions").then(function(response) {
+	$http.get("/api/listtransactions").then(function(response) {
 		console.log(response.data);
 		$scope.transactions = response.data.transactions;
 	});
 
-	let stakingLoad = $http.get("/api/getstakinginfo").then(function(response) {
+	$http.get("/api/getstakinginfo").then(function(response) {
 		console.log(response.data);
 
 		if(response.data.staking.staking) {
@@ -41,14 +37,6 @@ app.controller('WalletController', function($scope, $http) {
 		$scope.staking = response.data.staking;
 
 	});
-
-	loader.push(pingLoad, balanceLoad, accountsLoad, transactionsLoad, stakingLoad);
-
-	Promise.all(loader).then(() => {
-		$('#loader').fadeOut(150);
-	});
-
-
 
 });
 
